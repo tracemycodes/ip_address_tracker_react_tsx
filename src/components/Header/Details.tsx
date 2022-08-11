67348d6739525977eac2b8bf53e3984ed107bf50
Agba Emmanuel <agbaayoh@gmail.com>
2022-08-11 09:56:19 -0700

    typescript change event handler for from state

--- src/components/Header/Details.tsx
+++ src/components/Header/Details.tsx
@@ -1,32 +1,31 @@
-import React, {FC} from 'react';
-import './Head.module.css'
+import React, { FC } from 'react';
+import styles from './Head.module.css';
 
-export interface Props {
-}
+export interface Props {}
 
 const Details: FC = (props: Props) => {
   return (
     <>
-     <section>
-          <div>
-            <h2>IP Address</h2>
-            <p className='ip-num' />
-          </div>
-          <div>
-            <h2>Location</h2>
-            <p className='ip-location' />
-          </div>
-          <div>
-            <h2>Timezone</h2>
-            <p className='ip-timezone' />
-          </div>
-          <div>
-            <h2>ISP</h2>
-            <p className='ip-isp' />
-          </div>
-        </section> 
+      <section className={styles['details-section']}>
+        <div>
+          <h2>IP Address</h2>
+          <p className='ip-num' />
+        </div>
+        <div>
+          <h2>Location</h2>
+          <p className='ip-location' />
+        </div>
+        <div>
+          <h2>Timezone</h2>
+          <p className='ip-timezone' />
+        </div>
+        <div>
+          <h2>ISP</h2>
+          <p className='ip-isp' />
+        </div>
+      </section>
     </>
   );
-}
+};
 
-export default Details
+export default Details;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 // import MapInterface from '../mapBox/MapInterface';
 import Details from './Details';
 import './Head.module.css';
@@ -6,11 +6,23 @@ import './Head.module.css';
 interface IHeaderProps {}
 
 const Header: FC = (props: IHeaderProps) => {
+  const [text, setText] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <header>
       <h1>IP Address Tracker</h1>
       <form>
-        <input type='text' name='ip-address' id='ip-address' />
+        <input
+          type='text'
+          name='ip-address'
+          id='ip-address'
+          value={text}
+          onChange={handleChange}
+        />
         <button className='submit-btn'>
           <img src='./images/icon-arrow.svg' alt='+' />
         </button>
