73724bb70a7f708f863c70e46447fa64766aefc2
Agba Emmanuel <agbaayoh@gmail.com>
2022-08-17 09:38:48 -0700

    context file refactored

--- src/context/ipify/ipifyContext.ts
+++ src/context/ipify/ipifyContext.ts
@@ -1,17 +1,28 @@
 import { createContext } from "react"
 import { Dispatch, State } from "./IpifyState"
 
-type InitialStateType = {
-  state: State;
-  dispatch: Dispatch;
-}
+// type InitialStateType = {
+//   state: State;
+//   dispatch: Dispatch;
+// }
 
-const ipifyContext = createContext<InitialStateType | null>(null!)
+// const initialState = {
+//   loading: true,
+//   error: null,
+// };
+
+// const ipifyContext = createContext< {
+//   state: State;
+//   dispatch: React.Dispatch<any>;
+// } | null>({
+//   state: initialState,
+//   dispatch: () => null
+// })
 
 
-// const ipifyContext = createContext<{
-//   state: State
-//   dispatch: Dispatch
-// } | null>(null!)
+const ipifyContext = createContext<{
+  state: State
+  dispatch: Dispatch
+} | null>(null!)
 
 export default ipifyContext
