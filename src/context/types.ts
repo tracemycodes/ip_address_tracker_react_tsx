e1c168ce107813bb353127d45261974ef7b8274d
Agba Emmanuel <agbaayoh@gmail.com>
2022-08-22 08:25:49 -0700

    ipAddress check function

--- src/context/types.ts
+++
@@ -1,3 +0,0 @@
-export const GET_GEO_LOCATION: string = "GET_GEO_LOCATION"
-export const GEO_LOCATION_SUCCESS: string = "GEO_LOCATION_SUCCESS"
-export const GEO_LOCATION_ERROR: string = "GEO_LOCATION_ERROR"
